54cd6f70801d8ebf89441ee3fb6ad6a697a1c17d
jung-han <hanjung@nhnent.com>
2019-04-03 09:30:45 +0900

    chore: version up v2.2.3

--- karma.conf.js
+++ karma.conf.js
@@ -71,8 +71,8 @@ function setConfig(defaultConfig, server) {
             'IE8',
             'IE9',
             'IE10',
-            'IE11',
-            'Edge',
+            // 'IE11',
+            // 'Edge',
             'Chrome-WebDriver',
             'Firefox-WebDriver'
             // 'Safari-WebDriver'
