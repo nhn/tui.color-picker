4e94d7858312c88f1c55995d28a75dd392867219
Heena Kwag <heena.kwag@nhn.com>
2019-12-16 09:33:05 +0900

    chore: set webpack optimization

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,33 @@ var path = require('path');
 var pkg = require('./package.json');
 var webpack = require('webpack');
 var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var TerserPlugin = require('terser-webpack-plugin');
+var OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+function getOptimization(isMinified) {
+  if (isMinified) {
+    return {
+      minimizer: [
+        new TerserPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: false,
+          extractComments: false
+        }),
+        new OptimizeCSSAssetsPlugin()
+      ]
+    };
+  }
+
+  return {
+    minimize: false
+  };
+}
 
 module.exports = function(env, argv) {
   var isProduction = argv.mode === 'production';
-  var FILENAME = pkg.name + (isProduction ? '.min.js' : '.js');
-  var CSS_FILENAME = pkg.name + (isProduction ? '.min.css' : '.css');
+  var isMinified = !!argv.minify;
+  var FILENAME = pkg.name + (isMinified ? '.min' : '');
   var BANNER = [
     'TOAST UI Color Picker',
     '@version ' + pkg.version,
@@ -22,14 +44,14 @@ module.exports = function(env, argv) {
   ].join('\n');
 
   return {
-    mode: 'development',
+    mode: isProduction ? 'production' : 'development',
     entry: ['./src/styl/' + pkg.name + '.styl', './src/js/index.js'],
     output: {
       library: ['tui', 'colorPicker'],
       libraryTarget: 'umd',
       path: path.resolve(__dirname, 'dist'),
       publicPath: 'dist',
-      filename: FILENAME
+      filename: FILENAME + '.js'
     },
     externals: {
       'tui-code-snippet': {
@@ -67,9 +89,10 @@ module.exports = function(env, argv) {
       ]
     },
     plugins: [
-      new MiniCssExtractPlugin({filename: CSS_FILENAME}),
+      new MiniCssExtractPlugin({filename: FILENAME + '.css'}),
       new webpack.BannerPlugin(BANNER)
     ],
+    optimization: getOptimization(isMinified),
     devServer: {
       historyApiFallback: false,
       progress: true,
